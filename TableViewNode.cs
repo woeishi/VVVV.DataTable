63ebf0d3daeec2ebbb120993c24d7c3eec916e3c
tebjan <tebjan@vvvv.org>
2014-08-12 18:21:02 +0200

    non working state

--- TableViewNode.cs
+++ TableViewNode.cs
@@ -1,13 +1,14 @@
 #region usings
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
 
-using System.ComponentModel.Composition;
-using VVVV.PluginInterfaces.V2;
 using VVVV.Core.Logging;
+using VVVV.PluginInterfaces.V2;
 
-using System.Diagnostics;
 #endregion usings
 
 namespace VVVV.Nodes.Table
@@ -40,6 +41,7 @@ namespace VVVV.Nodes.Table
 		ILogger FLogger;
 
 		private DataGridView FDataGridView;
+		private FolderBrowserDialog folderBrowserDialog1;
 		private Table FData;
 		private bool FNeedsUpdate = false;
 		
@@ -77,6 +79,79 @@ namespace VVVV.Nodes.Table
 
 		void InitializeComponent()
 		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.FDataGridView = new System.Windows.Forms.DataGridView();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			((System.ComponentModel.ISupportInitialize)(this.FDataGridView)).BeginInit();
+			this.SuspendLayout();
+			
+			// 
+			// FDataGridView
+			// 
+			this.FDataGridView.AllowDrop = true;
+			this.FDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.FDataGridView.BackgroundColor = System.Drawing.Color.DimGray;
+			this.FDataGridView.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+			this.FDataGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+			dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle1.BackColor = System.Drawing.Color.DimGray;
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.FDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this.FDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+
+			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle2.BackColor = System.Drawing.Color.DimGray;
+			dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+			dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Black;
+			dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+			dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+			this.FDataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+			this.FDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.FDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+			this.FDataGridView.Location = new System.Drawing.Point(0, 0);
+			this.FDataGridView.Name = "FDataGridView";
+			this.FDataGridView.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+			dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle3.BackColor = System.Drawing.Color.DimGray;
+			dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+			dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.FDataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+			this.FDataGridView.Size = new System.Drawing.Size(563, 231);
+			this.FDataGridView.TabIndex = 0;
+			
+			this.FDataGridView.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView1CellMouseDown);
+			this.FDataGridView.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1CellValueChanged);
+			this.FDataGridView.ColumnAdded += new System.Windows.Forms.DataGridViewColumnEventHandler(this.DataGridView1ColumnAdded);
+			this.FDataGridView.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.DataGridView1DataError);
+			this.FDataGridView.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.DataGridView1UserDeletedRow);
+			this.FDataGridView.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+			this.FDataGridView.DragOver += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragOver);
+			this.FDataGridView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+			this.FDataGridView.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseMove);
+			
+
+			// 
+			// XMLGridViewControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.FDataGridView);
+			this.Name = "XMLGridViewControl";
+			this.Size = new System.Drawing.Size(563, 231);
+			((System.ComponentModel.ISupportInitialize)(this.FDataGridView)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.StateDataSet)).EndInit();
+			this.ResumeLayout(false);
+			
 			DataGridViewCellStyle defaultStyle = new DataGridViewCellStyle();
 			defaultStyle.SelectionBackColor = System.Drawing.Color.Gray;
 			defaultStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
@@ -116,7 +191,7 @@ namespace VVVV.Nodes.Table
 			this.FDataGridView.AlternatingRowsDefaultCellStyle = defaultAltStyle;
 			this.FDataGridView.DefaultCellStyle = new DataGridViewCellStyle();
 
-			this.FDataGridView.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
+			this.FDataGridView.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.FDataGridView_CellValidating);
 			this.FDataGridView.MouseMove += new System.Windows.Forms.MouseEventHandler(FDataGridView_MouseMove);
 			this.FDataGridView.ColumnAdded += new DataGridViewColumnEventHandler(TableViewNode_ColumnAdded);
 			// 
@@ -130,6 +205,234 @@ namespace VVVV.Nodes.Table
 			this.ResumeLayout(false);
 
 		}
+		
+		#region reorder
+		private Rectangle FDragBoxFromMouseDown;
+		private int FRowIndexFromMouseDown;
+		private int FRowIndexOfItemUnderMouseToDrop;
+		private void dataGridView1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+			{
+				// If the mouse moves outside the rectangle, start the drag.
+				if (FDragBoxFromMouseDown != Rectangle.Empty &&
+				    !FDragBoxFromMouseDown.Contains(e.X, e.Y))
+				{
+
+					// Proceed with the drag and drop, passing in the list item.
+					DragDropEffects dropEffect = FDataGridView.DoDragDrop(
+						FDataGridView.Rows[FRowIndexFromMouseDown],
+						DragDropEffects.Move);
+				}
+			}
+		}
+
+		private void dataGridView1_DragOver(object sender, DragEventArgs e)
+		{
+			e.Effect = DragDropEffects.Move;
+		}
+
+		private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+		{
+			// The mouse locations are relative to the screen, so they must be
+			// converted to client coordinates.
+			Point clientPoint = FDataGridView.PointToClient(new Point(e.X, e.Y));
+
+			// Get the row index of the item the mouse is below.
+			FRowIndexOfItemUnderMouseToDrop =
+				FDataGridView.HitTest(clientPoint.X, clientPoint.Y).RowIndex;
+
+			// If the drag operation was a move then remove and insert the row.
+			if (e.Effect== DragDropEffects.Move)
+			{
+				var foo = FDataGridView.DataSource as DataSet;
+				int idx = FDataGridView.SelectedRows[0].Index;
+				var value = foo.Tables[0].Rows[idx];
+				//clone
+				var newRow = foo.Tables[0].NewRow();
+				newRow.ItemArray = value.ItemArray;
+				foo.Tables[0].Rows.Remove(value);
+				foo.Tables[0].Rows.InsertAt(newRow, FRowIndexOfItemUnderMouseToDrop);
+				DoAutoSave();
+			}
+		}
+		
+		public event EventHandler<DataGridViewCellMouseEventArgs> TriggerButtonClicked;
+		
+		void DataGridView1CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+		{
+			if(e.ColumnIndex != -1)
+			{
+				if(e.Button == MouseButtons.Right)
+				{
+					var index = e.ColumnIndex - 1;
+					
+					if(index >= 0 && StateDataSet.Tables[0].Columns[index].DataType == typeof(string))
+					{
+						folderBrowserDialog1.SelectedPath = (string)FDataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value;
+						folderBrowserDialog1.ShowDialog(this);
+						FDataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = folderBrowserDialog1.SelectedPath;
+						return; // done
+					}
+				}
+				
+				if(e.Button == MouseButtons.Left)
+				{
+					if(FDataGridView.Columns[e.ColumnIndex].Name == "Trigger")
+					{
+						var handler = TriggerButtonClicked;
+						if(handler != null)
+						{
+							handler(this, e);
+						}
+						return; // done
+					}
+				}
+			}
+			
+			// Get the index of the item the mouse is below.
+			FRowIndexFromMouseDown = e.RowIndex;
+			if (FRowIndexFromMouseDown != -1)
+			{
+				// Remember the point where the mouse down occurred.
+				// The DragSize indicates the size that the mouse can move
+				// before a drag event should be started.
+				Size dragSize = SystemInformation.DragSize;
+
+				// Create a rectangle using the DragSize, with the mouse position being
+				// at the center of the rectangle.
+				FDragBoxFromMouseDown = new Rectangle(new Point(e.X - (dragSize.Width / 2),
+				                                               e.Y - (dragSize.Height / 2)),
+				                                     dragSize);
+			}
+			else
+				// Reset the rectangle if the mouse is not over an item in the ListBox.
+				FDragBoxFromMouseDown = Rectangle.Empty;
+		}
+		#endregion reorder
+		
+		#region copy/paste
+		private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+		{
+			try
+			{
+				if (e.Modifiers == Keys.Control)
+				{
+					switch (e.KeyCode)
+					{
+						case Keys.C:
+							CopyToClipboard();
+							break;
+
+						case Keys.V:
+							PasteClipboardValue();
+							break;
+							
+						case Keys.X:
+							CopyToClipboard();
+
+							//Clear selected cells
+							foreach (DataGridViewCell dgvCell in dataGridView1.SelectedCells)
+								dgvCell.Value = string.Empty;
+							break;
+							
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Copy/paste operation failed. "+ex.Message, "Copy/Paste", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+		}
+
+		private void CopyToClipboard()
+		{
+			//Copy to clipboard
+			DataObject dataObj = dataGridView1.GetClipboardContent();
+			if (dataObj != null)
+				Clipboard.SetDataObject(dataObj);
+		}
+
+		private void PasteClipboardValue()
+		{
+			dataGridView1.BeginEdit(false);
+			//Show Error if no cell is selected
+			if (dataGridView1.SelectedCells.Count == 0)
+			{
+				MessageBox.Show("Please select a cell", "Paste", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+
+			//Get the satring Cell
+			DataGridViewCell startCell = GetStartCell(dataGridView1);
+			//Get the clipboard value in a dictionary
+			Dictionary<int, Dictionary<int, string>> cbValue = ClipBoardValues(Clipboard.GetText());
+
+			int iRowIndex = startCell.RowIndex;
+			foreach (int rowKey in cbValue.Keys)
+			{
+				int iColIndex = startCell.ColumnIndex;
+				foreach (int cellKey in cbValue[rowKey].Keys)
+				{
+					//Check if the index is with in the limit
+					if (iColIndex <= dataGridView1.Columns.Count - 1 && iRowIndex <= dataGridView1.Rows.Count - 1)
+					{
+						DataGridViewCell cell = dataGridView1[iColIndex, iRowIndex];
+
+						cell.Value = cbValue[rowKey][cellKey];
+					}
+					iColIndex++;
+				}
+				iRowIndex++;
+			}
+			dataGridView1.EndEdit();
+		}
+
+		private DataGridViewCell GetStartCell(DataGridView dgView)
+		{
+			//get the smallest row,column index
+			if (dgView.SelectedCells.Count == 0)
+				return null;
+
+			int rowIndex = dgView.Rows.Count - 1;
+			int colIndex = dgView.Columns.Count - 1;
+
+			foreach (DataGridViewCell dgvCell in dgView.SelectedCells)
+			{
+				if (dgvCell.RowIndex < rowIndex)
+					rowIndex = dgvCell.RowIndex;
+				if (dgvCell.ColumnIndex < colIndex)
+					colIndex = dgvCell.ColumnIndex;
+			}
+
+			return dgView[colIndex, rowIndex];
+		}
+
+		private Dictionary<int, Dictionary<int, string>> ClipBoardValues(string clipboardValue)
+		{
+			Dictionary<int, Dictionary<int, string>> copyValues = new Dictionary<int, Dictionary<int, string>>();
+
+			String[] lines = clipboardValue.Split('\n');
+
+			for (int line = 0; line <= lines.Length - 1; line++)
+			{
+				copyValues[line] = new Dictionary<int, string>();
+				String[] lineContent = lines[line].Split('\t');
+
+				//if an empty cell value copied, then set the dictionay with an empty string
+				//else Set value to dictionary
+				if (lineContent.Length == 0)
+					copyValues[line][0] = string.Empty;
+				else
+				{
+					for (int j = 0; j <= lineContent.Length - 1; j++)
+						copyValues[line][j] = lineContent[j];
+				}
+			}
+			return copyValues;
+		}
+		
+		#endregion copy/paste
 		
 		#region Layout Stuff
 		void TableViewNode_ColumnAdded(object sender, DataGridViewColumnEventArgs e)
